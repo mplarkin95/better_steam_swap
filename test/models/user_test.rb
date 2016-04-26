66e37d56dd8dcf3b32e417c61ca8d1b8dc2752fc
mplarkin95 <mplarkin95@gmail.com>
2016-04-26 00:42:03 -0400

    Im an idiot and finally fixed tests we good bruh

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-  	@user = User.new(name = "Example",email = "fake123@stupid.com",steamid="420blaziet",password = "fake")\
+  	@user = User.new(name:  "Example",email:  "fake123@stupid.com",steam_id:"420blaziet",password_digest:  "fake")\
   end
 
   test "should be valid" do
-  	assert @user.valid
+  	assert @user.valid?
   end
 
 end
