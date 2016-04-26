1094c50d70aabd4e195c5dedcfd83ee249fa5cb5
mplarkin95 <mplarkin95@gmail.com>
2016-04-26 00:29:26 -0400

    fixed models of user and item

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :wishlists
-	has_many :items through :wishlists
+	has_many :items, through: :wishlists
 	has_many :inventorys
-	has_many :items through :inventorys
+	has_many :items, through: :inventorys
 end
+
