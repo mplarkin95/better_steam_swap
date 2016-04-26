8f0ad0a980776dd991f22204fbe4f46304cc7d9c
jonathancyr <jjcyr527@gmail.com>
2016-04-25 20:50:10 -0400

    Edited inventory.rb to include a method where if a game doesn't exist in the table then we add it

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,4 +1,12 @@
 class Inventory < ActiveRecord::Base
   belongs_to :items
   belongs_to :users
+
+  unless Inventory.where(:id => current_inventory.id).blank?
+    # Inventory.create(:column1 => 'data', :column2 => 'data') 
+    # What this does is that if Inventory does not have an entry in it's
+    # corresponding table then it'll add it into the table
+    # we can grab the data in the example line (first comment) by using the 
+    # ruby gem that I've made
+  end
 end
