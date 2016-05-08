6174bc157bd8fe6495d40a360e68a5c521ff7611
jonathancyr <jjcyr527@gmail.com>
2016-05-07 23:51:05 -0400

    Edited the controller to capture the users that have the current game you're looking at AND they have a game in their wishlist that you have - known as possible traders column. Changed the show page for items.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,8 +11,30 @@ class ItemsController < ApplicationController
 
   def show 
   	@item = Item.find(params[:id])
+
+    # Grabs all the users that have the current game
     user_ids = Inventory.where(item_id: @item.id).pluck(:user_id)
     @users = User.where(id: user_ids)
+
+    # Grabs all the users that have the current game and have a 
+    # game that you have in their wishlist
+    @traders = Array.new
+    user_games = Inventory.where(user_id: current_user.id).pluck(:item_id)
+    @current_games = Item.where(id: user_games)
+
+    for user in @users 
+      unless current_user == user 
+        wishlist = Wishlist.where(id: user.id).pluck(:item_id)
+        for game in @current_games
+          if wishlist.include?(game.id)
+            @traders << user 
+          end
+        end
+      end
+    end
+
   end
+
+
 
 end
