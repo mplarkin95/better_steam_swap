28d5b8442fc756537b38e2f3ee3da13e6f9e2fb6
jonathancyr <jjcyr527@gmail.com>
2016-05-08 23:52:44 -0400

    I improved the aesthetics of the website, I also updated searching and made paginate work for all parts

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,24 @@
 class SearchController < ApplicationController
-	def index
+
+  def index
     if params[:users]
     	@users = User.search(params[:advsearch]).order("name").paginate(:page => params[:page], :per_page => 6)
+      if @users.empty?
+        @users = false
+      end
     elsif params[:desc]
     	@items = Item.desc_search(params[:advsearch]).order("name").paginate(:page => params[:page], :per_page => 6)
+      if @items.empty?
+        @items = false
+      end
     elsif params[:advsearch] 
     	unless params[:users] and params[:desc]
     		@items = Item.search(params[:advsearch]).order("name").paginate(:page => params[:page], :per_page => 6)
     	end
+      if @items.empty?
+        @items = false
+      end
     end
   end
+
 end
